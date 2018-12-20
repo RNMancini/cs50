cfdf5d873ea1129bc60991b4478473f35e8b61b8
Rebecca <rebeccamancini.student@careerdevs.com>
2018-12-20 21:54:07 +0000

    class notes

---
+++ classNotes/struct0.c
@@ -0,0 +1,21 @@
+// Demonstrates structs
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "struct.h"
+
+int main(void)
+{
+    // allocate space for students
+    int enrollment = get_int("enrollment: ");
+    student students[enrollment];
+
+    // prompt for students' names and dorms
+    for (int i = 0; i < enrollment; i++)
+    {
+        printf("%s is in %s.\n", students[i].name, students[i].dorm);
+    }
+
+}
