518c60bb5274cac63758db3048216c96826071f6
Rebecca <rebeccamancini.student@careerdevs.com>
2019-02-08 01:10:10 +0000

    Pop

--- classwork/list.c
+++ classwork/list.c
@@ -19,5 +19,35 @@ int main(void)
 
    head->data = 5;
    head->next = second;
+   second->data = 7;
    second->next = third;
+   third->next = NULL;
+   third->data = 9
+}
+
+void addNode(intdata, struct Node* head)
+{
+  struct Node* trav = head;
+
+  while(trav->next != NULL)
+  {
+      trav = trav->next;
+  }
+
+    trav->next = (struct Node*)malloc(sizeof(struct Node))
+    trav->next->data = data
+    trav->next->next = NULL;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    trav->next = newNode;
+}
+
+int pop(struct Node* head)
+{
+    head->next = 5;
+    head->next->next = NULL;
+}
+{
+
 }
